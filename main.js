274301ae2ea9ebff13a3582fa3bf896bc3457277
Shvechka <steisysi@gmail.com>
2021-11-19 09:16:22 +0400

    const perdonslMuve

---
+++ main.js
@@ -0,0 +1,23 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const persanalMuveDB = {
+    cont: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+    persanalMuveDB.movies[a] = b; 
+    persanalMuveDB.movies[c] = d;   
+
+    console.log(persanalMuveDB);
+
+    
+
+
